32c76a494dcae9ce0f255be8033f8bd6cf3e6cc2
Your Name <you@example.com>
2026-01-21 08:57:23 -0300

    Comit inicial

---
+++ TimeTracker-Entevisual/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TimeTracker_Entevisual.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
